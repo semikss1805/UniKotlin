eed8ab3c1e124cd46b3095119a06e3b80b480bb4
semikss1805 <semikss1805fw@gmail.com>
2023-02-01 01:17:09 +0200

    lab_1: done

--- lab_1/app/src/main/java/com/example/lab_1/MainActivity.kt
+++ lab_1/app/src/main/java/com/example/lab_1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.lab_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+        button.setOnClickListener {
+            textView.text = getString(R.string.changedText)
+        }
     }
 }
